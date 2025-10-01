afe7c9019ec2285e7a065868884bc9f00e6ee465
ttymayor <lionhuang914903@gmail.com>
2025-10-01 22:32:24 +0800

    feat: create tty-middle layout component

---
+++ layouts/tty-middle.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Footer from "../components/Footer.vue";
+</script>
+
+<template>
+  <div class="slidev-layout tty-middle">
+    <div class="flex flex-col items-center justify-center">
+      <slot />
+    </div>
+  </div>
+  <Footer />
+</template>
+
+<style>
+.slidev-layout.tty-middle {
+  @apply flex flex-col items-center justify-center;
+}
+</style>
